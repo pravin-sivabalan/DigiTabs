9ddfcd87a7895223f01bd9e2bd1dc24a2d64d6be
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-01-03 23:03:43 -0600

    Add on create function Add function that iterates through tabs and change title to number

--- background.js
+++ background.js
@@ -1,18 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var print = chrome.extension.getBackgroundPage();
-  var submitButton = document.getElementById('submit');
-  submitButton.addEventListener('click', function(){
+// document.addEventListener('DOMContentLoaded', function() {
+//   var print = chrome.extension.getBackgroundPage();
+//   var submitButton = document.getElementById('submit');
+//
+//   submitButton.addEventListener('click', function(){
+//    chrome.tabs.query({}, function(tabs){
+//      for (var i = 0; i < tabs.length; i++) {
+//        chrome.tabs.executeScript(tabs[i].id,{code:"document.title = 'Pravin is Awesome'"});
+//      }
+//    });
+//
+//   }, false);
+// }, false);
 
-  // chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function(tab){
-  //     chrome.tabs.executeScript(tab.id,{code:"document.title = 'My lame title!'"});
-  //  });
+chrome.tabs.onCreated.addListener(function() {
+  chrome.tabs.query({currentWindow: true}, function(tabs){
+    for (var i = 0; i < tabs.length; i++) {
+      chrome.tabs.executeScript(tabs[i].id,{
+        code:"document.title = " + i
+      });
+    }
+  })
+});
 
-   chrome.tabs.query({}, function(tabs){
-     for (var i = 0; i < tabs.length; i++) {
-       print.console.log(tabs[i].id);
-       chrome.tabs.executeScript(tabs[i].id,{code:"document.title = 'Pravin is Awesome'"});
-     }
-   });
 
-  }, false);
-}, false);
+function changeTitle(title){
+  document.title = title
+}
