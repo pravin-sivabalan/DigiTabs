f4b461fdbdc924226c27b08f8c0c806d4a9c2931
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-01-04 00:07:14 -0600

    Fix second tab bug

--- background.js
+++ background.js
@@ -1,28 +1,34 @@
-numberTabs();
+chrome.windows.onCreated.addListener(function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tab){
+    chrome.tabs.executeScript(tab.id,{
+      code:"document.title = 1"
+    });
+  });
+});
+// TODO: fix bug where new window opening tab doesn't give number
 
 chrome.tabs.onMoved.addListener(function() {
   numberTabs();
 });
 
 chrome.tabs.onCreated.addListener(function() {
+  console.log("new tab");
   numberTabs();
 })
 
 function numberTabs() {
   chrome.tabs.query({currentWindow: true}, function(tabs){
-    for (var i = 1; i < tabs.length + 1; ++i) {
-      try {
-          console.log(i);
+    for (var i = 0; i < tabs.length + 1; ++i) {
+      // try {
+          console.log(tabs[i]);
+          var tabNum = i + 1;
           chrome.tabs.executeScript(tabs[i].id,{
-            code:"document.title = " + i
+            code:"document.title = "+tabNum+"."
           });
-      } catch (TypeError) {
-        console.log(i + "er");
-        continue;
-        // console.log("hello 2");
-        // i = i + 1;
-      }
-      console.log(i + "as");
+      // } catch (TypeError) {
+      //   console.log("hohoh");
+      //   continue;
+      // }
     }
   })
 }
