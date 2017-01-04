6737a9160ba7698018300f5f271dc3a11bf48bdc
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-01-03 23:21:06 -0600

    Add onMove listener Add function for iterating through tab and changing number

--- background.js
+++ background.js
@@ -1,28 +1,25 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   var print = chrome.extension.getBackgroundPage();
-//   var submitButton = document.getElementById('submit');
-//
-//   submitButton.addEventListener('click', function(){
-//    chrome.tabs.query({}, function(tabs){
-//      for (var i = 0; i < tabs.length; i++) {
-//        chrome.tabs.executeScript(tabs[i].id,{code:"document.title = 'Pravin is Awesome'"});
-//      }
-//    });
-//
-//   }, false);
-// }, false);
-
-chrome.tabs.onCreated.addListener(function() {
-  chrome.tabs.query({currentWindow: true}, function(tabs){
-    for (var i = 0; i < tabs.length; i++) {
-      chrome.tabs.executeScript(tabs[i].id,{
-        code:"document.title = " + i
-      });
-    }
-  })
+chrome.tabs.onMoved.addListener(function() {
+  // chrome.tabs.query({currentWindow: true}, function(tabs){
+  //   for (var i = 0; i < tabs.length; i++) {
+  //     chrome.tabs.executeScript(tabs[i].id,{
+  //       code:"document.title = " + i
+  //     });
+  //   }
+  // })
+  numberTabs();
 });
 
+chrome.tabs.onCreated.addListener(function() {
+  numberTabs();
+})
 
-function changeTitle(title){
-  document.title = title
+function numberTabs() {
+  chrome.tabs.query({currentWindow: true}, function(tabs){
+    for (var i = 0; i < tabs.length; i++) {
+      console.log(tabs[i]);
+      // chrome.tabs.executeScript(tabs[i].id,{
+      //   code:"document.title = " + i
+      // });
+    }
+  })
 }
